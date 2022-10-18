712d163abbec53957720ce4ead74d43a452861c0
abdulrehman2023 <abdulrehman@fabtechsol.com>
2022-10-18 04:16:53 -0700

    'opencv_merged_tested'

--- code.py
+++ code.py
@@ -3,46 +3,36 @@ import base64
 import time
 import os
 import codecs
-driver = webdriver.Chrome(executable_path="C:\\Users\\FabTechSol\\Downloads\\chromedriver_win32\\chromedriver.exe")
 from selenium.webdriver.common.by import By
 from threading import Thread
 from multiprocessing import Process
 import cv2
 import math
 from PIL import Image, ImageDraw
+import string
+import random
+from selenium.webdriver.chrome.options import Options
+import glob
+import numpy as np
 
+options = Options()
 
-driver.get("https://animalfactguide.com/")
+options.headless = True
+options.add_argument("window-size=1400,600")
+CHROMEDRIVER_PATH="C:\\Users\\FabTechSol\\Downloads\\chromedriver_win32\\chromedriver.exe"
+driver = webdriver.Chrome(CHROMEDRIVER_PATH, options=options)
+driver.get("https://doggies.com/")
 print("page loadded")
-time.sleep(10)
-# driver.fullscreen_window()
+time.sleep(15)
 
-# page_rect = driver.execute_cdp_cmd("Page.getLayoutMetrics", {})
-# print("matrices get")
-# time.sleep(5)
-# screenshot = driver.execute_cdp_cmd(
-#             "Page.captureScreenshot",
-#             {
-#                 "format": "png",
-#                 "captureBeyondViewport": True,
-#                 "clip": {
-#                     "width": page_rect["contentSize"]["width"],
-#                     "height": page_rect["contentSize"]["height"],
-#                     "x": 0,
-#                     "y": 0,
-#                     "scale": 1
-#                 }
-#             })
-# print("screenshot is taken")
 
-# with open('C:\\Users\\FabTechSol\\Desktop\\screenshot\\list\\new.png', "wb") as file:
-#     file.write(base64.urlsafe_b64decode(screenshot["data"]))
 
-# html = driver.page_source
-# n = os.path.join("C:\\Users\\FabTechSol\\Desktop\\screenshot\\html", "PageSave.html")
-# f = codecs.open(n, "w", "utf−8")
-# f.write(html)
-# driver.save_screenshot('screenshot.png')
+
+html = driver.page_source
+n = os.path.join("C:\\Users\\FabTechSol\\Desktop\\screenshot\\html", "PageSave.html")
+f = codecs.open(n, "w", "utf−8")
+f.write(html)
+
 
 getstyle = driver.find_elements(By.TAG_NAME, 'iframe')
 result = []
@@ -69,19 +59,26 @@ for style in getstyle:
         result.append(a)
         print(style.get_attribute("name"))
         print(style.get_attribute("height"))
-        print(style.get_attribute("width"))
+        width= style.get_attribute("width")
+        print(width)
+       
         print(style.get_attribute("style"))
-        content = driver.find_element(By.ID, a)
-        driver.execute_script("arguments[0].scrollIntoView(true);", content);
-        highlight(content, 5, 'red', 5)
-        
-        x,y = content.location.values()
-        h,w = content.size.values()
-        print(x,y,w, h)
-        img = Image.open('screenshot.png')
-        rimg = img.copy()
-        rimg.show()
-        rimg.save('pillow.png')
+        if width !='':
+            content = driver.find_element(By.ID, a)
+            driver = content._parent
+            driver.execute_script("arguments[0].scrollIntoView(true);", content);
+            highlight(content, 5, 'red', 1)
+            
+            x,y = content.location.values()
+            h,w = content.size.values()
+            print(x,y,w, h)
+            img = Image.open('screenshot.png')
+            rimg = img.copy()
+            
+            random_name = ''.join(random.choices(string.ascii_uppercase +
+                                string.digits, k=7))
+            saved_name='./scraped_images'+'/'+random_name+'.png'                     
+            rimg.save(saved_name)
         print("-------------------------------")
     elif "aswift" in iframe:
         print("-------------------------------")
@@ -90,19 +87,26 @@ for style in getstyle:
         result.append(a)
         print(style.get_attribute("name"))
         print(style.get_attribute("height"))
-        print(style.get_attribute("width"))
+        width=style.get_attribute("width")
         print(style.get_attribute("style"))
-        content = driver.find_element(By.ID, a)
-        driver.execute_script("arguments[0].scrollIntoView(true);", content);
-        highlight(content, 5, 'red', 5)
-        
-        x,y = content.location.values()
-        h,w = content.size.values()
-        print(x,y,w, h)
-        img = Image.open('screenshot.png')
-        rimg = img.copy()
-        rimg.show()
-        rimg.save('pillow.png')
+
+        print(width)
+        if width !='':
+            content = driver.find_element(By.ID, a)
+            driver = content._parent
+            driver.execute_script("arguments[0].scrollIntoView(true);", content);
+            highlight(content, 5, 'red', 1)
+            
+            x,y = content.location.values()
+            h,w = content.size.values()
+            print(x,y,w, h)
+            img = Image.open('screenshot.png')
+            rimg = img.copy()
+            
+            random_name = ''.join(random.choices(string.ascii_uppercase +
+                                string.digits, k=7))
+            saved_name='./scraped_images'+'/'+random_name+'.png'                     
+            rimg.save(saved_name)
         print("-------------------------------")
     else:
         print('no add found')
@@ -111,15 +115,49 @@ for style in getstyle:
 
 print(result)
 
+def opencv_handler(img):
+    # Read input image
+    img = cv2.imread(img)
+    img2 = cv2.imread('image.png')
+
+    # Gel all pixels in the image - where BGR = (34, 33, 33), OpenCV colors order is BGR not RGB
+    gray = np.all(img == (0, 0, 255), 2)  # gray is a logical matrix with True where BGR = (34, 33, 33).
+
+    # Convert logical matrix to uint8
+    gray = gray.astype(np.uint8)*255
+
+    # Find contours
+    cnts = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2]  # Use index [-2] to be compatible to OpenCV 3 and 4
+
+    # Get contour with maximum area
+    c = max(cnts, key=cv2.contourArea)
+
+    x, y, w, h = cv2.boundingRect(c)
+    print(x,y,w,h)
+    # Draw green rectangle for testing
+    # cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), thickness = 2)
+    resized_down = cv2.resize(img2, (w,h), interpolation= cv2.INTER_LINEAR)
+    cv2.imwrite('resize.png', resized_down)
+    # Show result
+    resize_image = cv2.imread('resize.png')
+    img[y:y+h,x:x+w]=resize_image
+    random_name = ''.join(random.choices(string.ascii_uppercase +
+                            string.digits, k=7))
+    saved_name='./results'+'/'+random_name+'.png'
+    cv2.imwrite(saved_name, img)
+    # cv2.imshow('gray', gray)
+    # cv2.imshow('img', img)
+    # cv2.imshow('img2', img2)
+    # cv2.imshow('img3', img3)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
 
-# content = driver.find_element(By.ID, result[0])
-# p1 = Process(target = highlight(content, 30, "red", 2))
-# p1.start()
-# content = driver.find_element(By.ID, result[1])
-# p2 = Process(target = highlight1(content, 30, "red", 2))
-# p2.start()
-# p1.join()
-# p2.join()
+
+screenshots =glob.glob("scraped_images*/*.png") 
+
+
+for image in screenshots:
+    opencv_handler(image)
 
 print("end")
--- code1.py
+++ code1.py
@@ -3,6 +3,7 @@ from io import BytesIO
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 def open_url(url):
 
@@ -13,62 +14,13 @@ def open_url(url):
     driver = webdriver.Chrome(CHROMEDRIVER_PATH, options=options)
     driver.get(url)
     time.sleep(15)
-    save_screenshot(driver, 'screen.png')
-
-def save_screenshot(driver, file_name):
-    height, width = scroll_down(driver)
-    driver.set_window_size(width, height)
-    img_binary = driver.get_screenshot_as_png()
-    img = Image.open(BytesIO(img_binary))
-    img.save(file_name)
-    # print(file_name)
-    print(" screenshot saved ")
+    driver.fullscreen_window()
+    driver.save_screenshot('t1.png')
+    driver = driver.find_element(By.TAG_NAME, "body")
+    driver.save_screenshot('t2.png')
+    print("end")
 
 
-def scroll_down(driver):
-    total_width = driver.execute_script("return document.body.parentNode.scrollWidth")
-    total_height = driver.execute_script("return document.body.parentNode.scrollHeight")
-    viewport_width = driver.execute_script("return document.body.clientWidth")
-    viewport_height = driver.execute_script("return window.innerHeight")
 
-    rectangles = []
-
-    i = 0
-    while i < total_height:
-        ii = 0
-        top_height = i + viewport_height
-
-        if top_height > total_height:
-            top_height = total_height
-
-        while ii < total_width:
-            top_width = ii + viewport_width
-
-            if top_width > total_width:
-                top_width = total_width
-
-            rectangles.append((ii, i, top_width, top_height))
-
-            ii = ii + viewport_width
-
-        i = i + viewport_height
-
-    previous = None
-    part = 0
-
-    for rectangle in rectangles:
-        if not previous is None:
-            driver.execute_script("window.scrollTo({0}, {1})".format(rectangle[0], rectangle[1]))
-            time.sleep(0.5)
-        # time.sleep(0.2)
-
-        if rectangle[1] + viewport_height > total_height:
-            offset = (rectangle[0], total_height - viewport_height)
-        else:
-            offset = (rectangle[0], rectangle[1])
-
-        previous = rectangle
-
-    return (total_height, total_width)
 
 open_url("https://babyanimalz.com/")
